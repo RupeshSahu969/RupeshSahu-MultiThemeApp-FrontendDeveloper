ed0f65b411bbc713b8470fdb17ba8b8e68e76eb4
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-08-04 00:00:12 +0530

    add All Rotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Header />
+      <main className="pt-16">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
